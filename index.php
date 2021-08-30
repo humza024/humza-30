4b2113ee95dacf814ff964352511601e6fe16898
humza024 <88472995+humza024@users.noreply.github.com>
2021-08-30 12:01:50 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <title>GitHub Actions with Docker container</title>
 </head>
 <?php
-echo "PHP Code ok";
+echo "This is notfair ";
 ?>
 <body>
 <h1>My name is humza Arshad khan</h1>
-<p>I am a DevOps Engineer</p>
+<p>I am a DevOps Engineer at galaxy solutions itc</p>
 </body>
 </html>
